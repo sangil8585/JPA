9f319d0ac351660e7fc7a43727bd2544b55dbb98
sangil <sangil8585@naver.com>
2024-08-09 08:13:06 +0900

    주문 조회 v5

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -80,6 +80,16 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> ordersV6() {
+        return orderQueryRepository.findAllByDto_flat();
+    }
+
     @Data
     static class OrderDto {
 

468005e644c16f4392d3af0b0d7281d2c12f17ef
sangil <sangil8585@naver.com>
2024-08-09 08:39:41 +0900

    주문 조회 v6

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -73,7 +73,14 @@ public class OrderQueryRepository {
                 .getResultList();
     }
 
-    public List<OrderQueryDto> findAllByDto_flat() {
-
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery(
+                "select new jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.orderPrice)" +
+                        " from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d" +
+                        " join o.orderItems oi" +
+                        " join oi.item i", OrderFlatDto.class)
+                .getResultList();
     }
 }

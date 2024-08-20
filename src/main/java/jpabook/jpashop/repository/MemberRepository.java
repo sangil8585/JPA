ae471ad33e9967e3870ed8e71d71ba9564f0dbcb
sangil <sangil8585@naver.com>
2024-08-20 23:46:25 +0900

    스프링 데이터 JPA 기능 추가

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -7,8 +7,9 @@ import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    // findByName이라고 되어있으면 
+    // findByName이라고 되어있으면 구현체를 만들어야 되는게 일반적이지만 스프링 데이터 JPA는
     // select m from Mmeber m where m.name = ?
     // name을 보고 알아서 위에 쿼리를 만듬 
     List<Member> findByName(String name);
+
 }
